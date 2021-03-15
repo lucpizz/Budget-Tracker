d21b81bf769914706374bc23638211f6ba9f0a12
lucpizz <Lucpizz@gmail.com>
2021-03-15 09:52:04 -0400

    adding service-worker script to index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,14 @@
+
+
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/index.js",
-  "/styles.css",
-  "/manifest.webmanifest",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
-  "/db.js",
+  "./",
+  "./index.html",
+  "./index.js",
+  "./styles.css",
+  "./manifest.webmanifest",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "./db.js",
 ];
 
 const PRECACHE = "precache-v1";
